642bde4703527310217652a8f781e9319a4adec1
andyfordBSU <andyford@u.boisestate.edu>
2025-09-02 20:35:58 +0000

    lecture coding example

--- MyApp/main.c
+++ MyApp/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#define TENYEAR 10
+#define TENYEARAGO(x) x-10
 
 int main(void) {
 
-   /* Your code here */
+   int year;
+   printf("Enter the year: ");
+   scanf("%d", &year);
+   printf("This year is: %d\n", year);
+   printf("10 years ago is: %d.\n", year-10);
+   printf("10 years ago is: %d.\n", year-TENYEAR);
+   printf("10 years ago is: %d.\n", TENYEARAGO(year));
+
+   printf("The address of variable year: %p.\n", &year);
+
+
 
    return 0;
 }
